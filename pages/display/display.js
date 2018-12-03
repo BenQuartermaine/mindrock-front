44a9e821a32f6b6969138e3854dad1b0943cfa68
claire chen <clairewenhuang.chen@gmail.com>
2018-12-03 12:59:50 +0800

    display page

--- pages/display/display.js
+++ pages/display/display.js
@@ -5,13 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    photo: [
+      '/img/1.jpg',
+      '/img/2.jpg',
+      '/img/4.jpg',
+      '/img/5.jpg',
+      '/img/6.jpg',
+      '/img/7.jpg',
+      '/img/8.jpg',
+    ]
   },
 
+  getDisplayData() {
+    const userId = wx.getStorageSync("userId")
+
+    const app = getApp()
+    const dev = app.globalData.dev
+    const prod = app.globalData.prod
+
+    const page = this
+  },
+    // wx.request({
+    //   url: prod + `api/v1/users/${userId}`,
+    //   method: "GET",
+    //   success(res) {
+    //     page.setData(
+    //       res.data
+    //     )
+    //   }
+    // })
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function () {
+    console.log()
+    this.getDisplayData()
+    const userInfo = wx.getStorageSync("userInfo")
+    this.setData({
+      avatar: userInfo.avatarUrl
+    })
+    console.log(this.data)
 
   },
 
@@ -26,7 +59,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
